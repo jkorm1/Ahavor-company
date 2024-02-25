9774b6b2efef4ecabe6a42ba6bce4e92a73645ab
jkorm1 <josephkorm1@gmail.com>
2024-02-25 18:39:39 +0000

    Add files via upload

---
+++ Ahava.js
@@ -0,0 +1,30 @@
+var marquee = document.querySelector('marquee');
+
+function stop() {
+  marquee.stop();
+}
+
+function start() {
+  marquee.start();
+}
+
+function displayPop() {
+  document.getElementById('popup') .style.display = 'block';
+}
+
+function closePop() {
+  document.getElementById('popup') .style.display = 'none'
+}
+
+function learnMore() {
+  document.getElementById('appear') .style.display = 'block'
+}
+
+function times() {
+  document.getElementById('appear') .style.display = 'none'
+}
+
+
+
+
+
